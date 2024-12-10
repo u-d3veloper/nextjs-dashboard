ccbd408ed26985d4525247e20629308e5e9c054b
Titouan Auterioux <titouan@MacBook-Air-de-Titouan.local>
2024-12-10 19:34:06 +0100

    logo removal

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -9,7 +8,9 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+        <h1 className={`${lusitana.className} antialiased text-3xl font-bold text-white md:text-5xl`}>
+          Acme
+        </h1>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
